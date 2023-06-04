34c5785456c1ccee1504cd75486cff4827eb5c43
Isaac Nelson <ihmnelson@outlook.com>
2023-06-04 13:54:25 -0700

    yesyseyyesysey

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -10,14 +10,24 @@ const Skills = () => {
               <p className='py-4 text-2xl'>Here are what people across the interent are currently saying about the problem.</p>
           </div> 
           <div className='w-full grid grid-cols-2 sm:grid-cols-2 gap-4 text-center py-8'>
-            <a href="https://www.google.com/">
-              <div className='bg-[#6763e7] shadow-md shadow-[#5752e5] hover:scale-110 duration-500'>
-                  <p className='my-4'>"Put a quote here!"</p>
+          <a href="https://www.bbc.com/news/science-environment-63037553">
+          <div className='bg-[#6763e7] shadow-md shadow-[#5752e5] hover:scale-110 duration-500'>
+                  <p className='my-4'>"Should e-cigarettes be banned?"</p>
               </div>
               </a>
-              <a href="https://www.google.com/">
+              <a href="https://greenerideal.com/news/vaping-liquid-nicotine-can-kill-the-environment/">
               <div className='bg-[#6763e7] shadow-md shadow-[#5752e5] hover:scale-110 duration-500'>
-                  <p className='my-4'>"Put another quote from somewhere else here!"</p>
+                  <p className='my-4'>"Liquid nicotine hurts the environment"</p>
+              </div>
+              </a>
+              <a href="https://www.undo.org/environmental-impact/vape-waste-is-toxic-waste">
+              <div className='bg-[#6763e7] shadow-md shadow-[#5752e5] hover:scale-110 duration-500'>
+                  <p className='my-4'>"Vapes: 3 types of toxic waste in 1"</p>
+              </div>
+              </a>
+              <a href="https://truthinitiative.org/research-resources/harmful-effects-tobacco/toxic-plastic-problem-e-cigarette-waste-and-environment">
+              <div className='bg-[#6763e7] shadow-md shadow-[#5752e5] hover:scale-110 duration-500'>
+                  <p className='my-4'>"A toxic, plastic problem"</p>
               </div>
               </a>
           </div>
